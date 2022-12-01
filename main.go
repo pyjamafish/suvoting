b11bc16bd619d972714a5d74d07cf3ecdd4eae20
Fisher Sun <f.sun@wustl.edu>
2022-11-30 23:44:06 -0600

    Get main.go to work!
    Now we can serve the react files with:
    ```
    go run .
    ```

--- main.go
+++ main.go
@@ -1,22 +1,64 @@
+// Adapted from https://github.com/go-chi/chi/issues/403#issuecomment-469152247
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
-	"log"
+	"fmt"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
 )
 
+const port = ":3333"
+
 func main() {
-	// 1. Create a new router
+	fmt.Printf("Starting Server on Port %v\n", port)
 	router := chi.NewRouter()
-
-	// 2. Register an endpoint
-	fileServer := http.FileServer(http.Dir("/client/build"))
-	router.Handle("/*", http.StripPrefix("/client/build/", fileServer))
-
-	// 3. Use router to start the server
-	err := http.ListenAndServe(":3000", router)
-	if err != nil {
-		log.Println(err)
+	server := &http.Server{
+		Addr:         port,
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
+
+	router.Get("/foo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{ "message": "bar" }`))
+	})
+
+	FileServer(router, "/", "client/build/")
+
+	panic(server.ListenAndServe())
+}
+
+// FileServer is serving static files
+func FileServer(r chi.Router, public string, static string) {
+	if strings.ContainsAny(public, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	root, _ := filepath.Abs(static)
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		panic("Static Documents Directory Not Found")
+	}
+
+	fs := http.StripPrefix(public, http.FileServer(http.Dir(root)))
+
+	if public != "/" && public[len(public)-1] != '/' {
+		r.Get(public, http.RedirectHandler(public+"/", 301).ServeHTTP)
+		public += "/"
+	}
+
+	r.Get(public+"*", func(w http.ResponseWriter, r *http.Request) {
+		file := strings.Replace(r.RequestURI, public, "/", 1)
+		if _, err := os.Stat(root + file); os.IsNotExist(err) {
+			http.ServeFile(w, r, path.Join(root, "index.html"))
+			return
+		}
+		fs.ServeHTTP(w, r)
+	})
 }
