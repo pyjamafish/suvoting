7e68c50a2578df61525b474887fc1357fd0238cd
Fisher Sun <f.sun@wustl.edu>
2022-11-30 23:57:30 -0600

    Simplify code

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi/v5"
@@ -30,35 +26,8 @@ func main() {
 		w.Write([]byte(`{ "message": "bar" }`))
 	})
 
-	FileServer(router, "/", "client/build/")
+	fileServer := http.FileServer(http.Dir("./client/build/"))
+	router.Handle("/*", fileServer)
 
 	panic(server.ListenAndServe())
-}
-
-// FileServer is serving static files
-func FileServer(r chi.Router, public string, static string) {
-	if strings.ContainsAny(public, "{}*") {
-		panic("FileServer does not permit URL parameters.")
-	}
-
-	root, _ := filepath.Abs(static)
-	if _, err := os.Stat(root); os.IsNotExist(err) {
-		panic("Static Documents Directory Not Found")
-	}
-
-	fs := http.StripPrefix(public, http.FileServer(http.Dir(root)))
-
-	if public != "/" && public[len(public)-1] != '/' {
-		r.Get(public, http.RedirectHandler(public+"/", 301).ServeHTTP)
-		public += "/"
-	}
-
-	r.Get(public+"*", func(w http.ResponseWriter, r *http.Request) {
-		file := strings.Replace(r.RequestURI, public, "/", 1)
-		if _, err := os.Stat(root + file); os.IsNotExist(err) {
-			http.ServeFile(w, r, path.Join(root, "index.html"))
-			return
-		}
-		fs.ServeHTTP(w, r)
-	})
 }
