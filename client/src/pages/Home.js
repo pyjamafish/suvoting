7545a9ec2880d6e59570a1b92c36853a450825f9
harrygao56 <harrygao56@gmail.com>
2022-12-05 15:29:01 -0600

    made pages and styling

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+// Contains code for main landing page
+
+import "../styling/Home.css"
+
+const Home = () => {
+    return (
+    <h1 className="main-header">Welcome to the Student Union Elections Website</h1>
+    );
+};
+  
+export default Home;
---
+++ client/src/pages/Vote.js
@@ -0,0 +1,5 @@
+const Vote = () => {
+    return <h1>Vote</h1>;
+};
+  
+export default Vote;
