6c836505e38e95d5d4307ffcc99151b6cc00286d
Fisher Sun <f.sun@wustl.edu>
2022-12-06 17:20:43 -0600

    Define api models

--- server/models.go
+++ server/models.go
@@ -1,10 +1,21 @@
 package server
 
 type Candidate struct {
+	Id      int32    `json:"id"`
+	Name    string   `json:"name"`
+	Votes   int32    `json:"votes"`
+	Answers []string `json:"answers"`
 }
 
 type LeaderboardEntry struct {
+	Id    int32  `json:"id"`
+	Name  string `json:"name"`
+	Votes int32  `json:"votes"`
 }
 
 type Answer struct {
+	Id     int32  `json:"id"`
+	Name   string `json:"name"`
+	Votes  int32  `json:"votes"`
+	Answer string `json:"answer"`
 }
