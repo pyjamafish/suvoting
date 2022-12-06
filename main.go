8805837c3d7da9a6240ef68ea9f85e845c3c2172
Fisher Sun <f.sun@wustl.edu>
2022-12-06 16:31:53 -0600

    Revert "Use actual hostname instead of hard coding localhost"
    This reverts commit 521369aa138b304a5b0087f323b029c4533003c3.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
-	"os"
 	"vote/server"
 )
 
@@ -13,12 +12,8 @@ func main() {
 	rs := server.NewAppResource()
 	defer rs.Close()
 
-	hostname, err := os.Hostname()
-	if err != nil {
-		panic(err)
-	}
 	port := ":3456"
-	fmt.Printf("Now serving! http://%s%s\n", hostname, port)
+	fmt.Printf("Now serving! http://localhost%s\n", port)
 
 	// 1. Create a new router
 	router := chi.NewRouter()
@@ -29,7 +24,7 @@ func main() {
 	router.Get("/api", rs.Api)
 
 	// 3. Use router to start the server
-	err = http.ListenAndServe(port, router)
+	err := http.ListenAndServe(port, router)
 	if err != nil {
 		log.Println(err)
 	}
