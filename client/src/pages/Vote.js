f7530d7b452e546f3129e2d7c792adf95cfb059e
harrygao56 <harrygao56@gmail.com>
2022-12-07 02:20:51 -0600

    changes

--- client/src/pages/Vote.js
+++ client/src/pages/Vote.js
@@ -1,44 +1,140 @@
 // Contains code for vote page
 
 import "../styling/Vote.css";
+import "../components/Candidate.js";
 import React from 'react';
-import { Link } from "react-router-dom";
+
 
 class Vote extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            candidates: []
+            senateCandidates: [],
+            treasuryCandidates: []
         };
     }
 
     componentDidMount() {
-        fetch("http://ec2-54-243-4-131.compute-1.amazonaws.com:3456/api/senate/candidates", {
-            method: "POST",
-            headers: {
-				"Content-Type": "application/json",
-			},
-            mode: 'cors'
-        })
+        fetch("http://ec2-54-243-4-131.compute-1.amazonaws.com:3456/api/senate/candidates")
         .then(res => res.json())
         .then(
-        (result) => {
-        //   this.setState({
-        //     candidates: result.candidates
-        //   });
-            console.log(result.candidates);
-        },
-        (error) => {
-          console.log("Error retrieving candidates");
-        }
-      )
+            (result) => {
+                this.setState({
+                    senateCandidates: result.data.candidates
+                });
+            },
+            (error) => {
+                console.log("Error retrieving candidates");
+            }
+        )
+
+        fetch("http://ec2-54-243-4-131.compute-1.amazonaws.com:3456/api/treasury/candidates")
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(result.data.candidates);
+                this.setState({
+                    treasuryCandidates: result.data.candidates
+                });
+            },
+            (error) => {
+                console.log("Error retrieving candidates");
+            }
+        )
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
     }
 
     render() {
         return(
-            <p>Vote</p>
+            <div id="vote-container">
+                <div id="cand-panel">
+                    <h1>Candidate Information</h1>
+                    <h2>Senate</h2>
+                    {this.state.senateCandidates.length !== 0 ? (
+                        <div className="candidate-container">
+                            {this.state.senateCandidates.map((candidate) => (
+                                <Candidate
+                                    name={candidate.name}
+                                    answers={candidate.answers}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <p>No Senate Candidates</p>
+                    )}
+                    
+                    <h2>Treasury</h2>
+                    {this.state.treasuryCandidates.length !== 0 ? (
+                        <div className="candidate-container">
+                            {this.state.treasuryCandidates.map((candidate) => (
+                                <Candidate
+                                    name={candidate.name}
+                                    answers={candidate.answers}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <p>No Treasury Candidates</p>
+                    )}
+                </div>
+                <div id="vote-panel">
+                    <h1>Vote</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <h2>Senate</h2>
+                        {this.state.senateCandidates.length !== 0 ? (
+                            <div>
+                                {this.state.senateCandidates.map((candidate) => (
+                                    <div>
+                                        <input type="checkbox" name="senate-pick" value={candidate._id}/>
+                                        <label for={candidate._id}>{candidate.name}</label>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No Senate Candidates</p>
+                        )}
+                        <h2>Treasury</h2>
+                        {this.state.treasuryCandidates.length !== 0 ? (
+                            <div>
+                                {this.state.treasuryCandidates.map((candidate) => (
+                                    <div>
+                                        <input type="checkbox" name="treasury-pick" value={candidate._id}/>
+                                        <label for={candidate._id}>{candidate.name}</label>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No Treasury Candidates</p>
+                        )}
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </div>
         );
     }
 };
+
+function Candidate(props) {
+	return (
+        <div className="candidate-div">
+            <h3 className="candidate-name">
+                {props.name}
+            </h3>
+            <ul>
+                {props.answers.map((answer) => (
+                    <li>
+                        {answer}
+                    </li>
+                ))}
+            </ul>
+            <p>
+                {props.responses}
+            </p>
+        </div>
+    );
+}
 
 export default Vote;
