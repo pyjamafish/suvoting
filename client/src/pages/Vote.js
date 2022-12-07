55d7b836f1271c7149f82676d47ec703ea49d48b
harrygao56 <harrygao56@gmail.com>
2022-12-06 23:44:01 -0600

    vote

--- client/src/pages/Vote.js
+++ client/src/pages/Vote.js
@@ -1,5 +1,44 @@
-const Vote = () => {
-    return <h1>Vote</h1>;
+// Contains code for vote page
+
+import "../styling/Vote.css";
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class Vote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            candidates: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("http://ec2-54-243-4-131.compute-1.amazonaws.com:3456/api/senate/candidates", {
+            method: "POST",
+            headers: {
+				"Content-Type": "application/json",
+			},
+            mode: 'cors'
+        })
+        .then(res => res.json())
+        .then(
+        (result) => {
+        //   this.setState({
+        //     candidates: result.candidates
+        //   });
+            console.log(result.candidates);
+        },
+        (error) => {
+          console.log("Error retrieving candidates");
+        }
+      )
+    }
+
+    render() {
+        return(
+            <p>Vote</p>
+        );
+    }
 };
-  
+
 export default Vote;
